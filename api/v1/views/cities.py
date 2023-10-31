2c2bdacde40e03bb073a92b4df9b0ab798c22f2c
10thcode <attabadukwabena@gmail.com>
2023-10-31 20:15:16 +0000

    Fix issues in cities.py api

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,24 +6,34 @@ from models.city import City
 from api.v1.views import app_views
 
 
-@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["GET"])
-@app_views.route("/cities", strict_slashes=False, methods=["GET"])
-def get_cities(city_id=None):
+@app_views.route("/states/<state_id>/cities",
+                 strict_slashes=False, methods=["GET"])
+def get_state_cities(state_id):
+    """ get all state cities objects """
+    id = "State.{}".format(state_id)
+    if id in storage.all():
+        city_objs = storage.all(City)
+        city_list = []
+
+        for city_obj in city_objs:
+            if city_objs[city_obj].state_id == state_id:
+                city_list.append(city_objs[city_obj].to_dict())
+
+        return jsonify(city_list)
+
+    abort(404)
+
+
+@app_views.route("/cities/<city_id>", strict_slashes=False,
+                 methods=["GET"])
+def get_cities(city_id):
     """ get all city objects """
     city_objs = storage.all(City)
 
-    if city_id:
-        for city_obj in city_objs:
-            if city_obj.split(".")[1] == city_id:
-                return jsonify(city_objs[city_obj].to_dict())
-        abort(404)
-
-    city_list = []
-
     for city_obj in city_objs:
-        city_list.append(city_objs[city_obj].to_dict())
-
-    return jsonify(city_list)
+        if city_obj.split(".")[1] == city_id:
+            return jsonify(city_objs[city_obj].to_dict())
+    abort(404)
 
 
 @app_views.route("/cities/<city_id>", strict_slashes=False,
@@ -40,19 +50,24 @@ def delete_cities(city_id):
     abort(404)
 
 
-@app_views.route("/cities", strict_slashes=False, methods=["POST"])
-def post_cities():
+@app_views.route("/states/<state_id>/cities", strict_slashes=False,
+                 methods=["POST"])
+def post_cities(state_id):
     """ create new city objects """
-    if request.mimetype == "application/json":
-        json_data = request.get_json()
-        if "name" in json_data:
-            city = City(**json_data)
-            city.save()
-            return make_response(jsonify(city.to_dict()), 201)
+    id = "State.{}".format(state_id)
+    if id in storage.all():
+        if request.mimetype == "application/json":
+            json_data = request.get_json()
+            if "name" in json_data:
+                json_data["state_id"] = state_id
+                city = City(**json_data)
+                city.save()
+                return make_response(jsonify(city.to_dict()), 201)
+            else:
+                abort(400, "Missing name")
         else:
-            abort(400, "Missing name")
-    else:
-        abort(400, "Not a JSON")
+            abort(400, "Not a JSON")
+    abort(404)
 
 
 @app_views.route("/cities/<city_id>", strict_slashes=False, methods=["PUT"])
